9ab78de3c904ba3173518434fa29b2aaeb6a396d
load0ne <jjazan201@gmail.com>
2020-04-16 16:37:29 +0900

    [cpp] binary search tree - search
    - /tree/BinarySearchTree
    - compiler: gcc 9.2v

--- tree/BinarySearchTree.cpp
+++ tree/BinarySearchTree.cpp
@@ -14,7 +14,7 @@ class BinarySearchTree;
 template <typename Data>
 class Node {
     friend class BinarySearchTree<Data>;
-private:
+public:
     Data _data;
     Node *_left;
     Node *_right;
@@ -29,7 +29,7 @@ public:
 
 template <typename Data>
 class BinarySearchTree {
-private:
+public:
     Node<Data> *_root;
     int size;
 
@@ -39,12 +39,12 @@ public:
         size = 1;
     }
 
-    // O(logN) Time Complexity using Big-O
+    // O(H) Time Complexity using Big-O
     void insert(const Data& data) {
         Node<Data> *cursor = _root;
         
         while (true) {
-            bool isBigger = cursor->_data >= data;
+            bool isBigger = data >= cursor->_data;
             Node<Data> *next = isBigger ? cursor->_right : cursor->_left;
 
             if (next) {
@@ -62,17 +62,55 @@ public:
 
         size++;
     }
+
+    // O(H) Time Complexity using Big-O
+    void searchAndPrintPath(const Data& data) {
+        Node<Data> *cursor = _root;
+        vector<Data> paths;
+
+        while (cursor) {
+            Data currentData = cursor->_data;
+
+            paths.push_back(currentData);
+            if (currentData == data)
+                break;
+
+            bool isBigger = data >= currentData;
+            cursor = isBigger ? cursor->_right : cursor->_left;
+        }
+
+        unsigned int pathsSize = paths.size();
+
+        cout << "Search Path of " << data << ": ";
+
+        for (unsigned int i = 0; i < pathsSize; ++i) {
+            if (pathsSize - 1 > i)
+                cout << paths[i] << " > ";
+            else 
+                cout << paths[i] << endl;
+        }
+    }
 };
 
 int main() {
     BinarySearchTree<int> tree = BinarySearchTree<int>{ 30 };
 
+    //           30
+    //    10            45
+    // 5     25     35       60
     tree.insert(10);
     tree.insert(45);
     tree.insert(25);
     tree.insert(5);
     tree.insert(60);
     tree.insert(35);
+   
+    // 30 > 45 > 35 
+    tree.searchAndPrintPath(35);
+    // 30 > 10 > 5
+    tree.searchAndPrintPath(5);
+    // 30 > 10 > 25
+    tree.searchAndPrintPath(25);
     
     return 0;
 }
